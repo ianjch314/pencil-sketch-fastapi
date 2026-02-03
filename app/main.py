55242b4ebdee8348ce50306830245727a1973514
ianjch314 <204469360+ianjch314@users.noreply.github.com>
2026-02-02 21:32:49 -0500

    Complete pencil sketch effect

--- app/main.py
+++ app/main.py
@@ -17,14 +17,18 @@ async def process_image(file: UploadFile):
     # Convert NumPy array to OpenCV image
     img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
 
-    # Convert color image to grayscale
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # Perform manual pencil sketch effect
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img_inverted = 255 - img_gray
+    img_inverted_blur = cv2.GaussianBlur(img_inverted, (21, 21), 0)
+    img_inverted_blur = 255 - img_inverted_blur
+    img_pencil_sketch = img_gray/img_inverted_blur*255
 
     # Output image to file
     filename_raw = file.filename.split('.')
-    filename_gray = f'{filename_raw[0]}_gray.{filename_raw[1]}'
-    cv2.imwrite(f'processed_img/{filename_gray}', img)
+    filename_pencil_sketch = f'{filename_raw[0]}_pencil_sketch.{filename_raw[1]}'
+    cv2.imwrite(f'processed_img/{filename_pencil_sketch}', img_pencil_sketch)
 
     return FileResponse(
-        path=f'processed_img/{filename_gray}',
-        filename=filename_gray)
+        path=f'processed_img/{filename_pencil_sketch}',
+        filename=filename_pencil_sketch)
